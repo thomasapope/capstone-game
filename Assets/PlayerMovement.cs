5f942e640a14b59a7a456aac7430d8f06df2df0c
Thomas Pope <45110100+thomasapope@users.noreply.github.com>
2020-02-04 16:17:10 -0600

    Initiallize Unity Project

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 10f;
+    public Vector3 inputVector;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+       inputVector = new Vector3 (Input.GetAxisRaw("Horizontal") * speed, rb.velocity.y, Input.GetAxisRaw("Vertical") * speed).normalized * 8;
+    //    Vector3 direction = focus.position - transform.position;
+       rb.velocity = inputVector;
+    }
+
+}
