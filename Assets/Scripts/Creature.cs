8b84540af34392b72469a635d2276fd6d457fd52
Thomas Pope <45110100+thomasapope@users.noreply.github.com>
2020-02-06 10:28:24 -0600

    Create Creature superclass
    Created a creature superclass that player and enemy classes will inherit from. Moved current movement code into it.

---
+++ Assets/Scripts/Creature.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    Creature.cs
+
+    The superclass of all creatures and players. This is not meant to be used
+    as is, but to be inherited from.
+*/
+
+public abstract class Creature : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    protected Vector3 inputVector;
+
+    public float speed = 10f;
+
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+    
+    
+    protected virtual void Update()
+    // Runs every frame. Virtual so that subclasses can override it.
+    {
+        Move();
+    }
+
+
+    void Move() {
+        // Logic necessary for moving the creature.
+        // Uses the inputVector given it by the subclass and moves based on it.
+        // This allows us to create movement behavior once and reuse it for 
+        // most or all players and enemies.
+
+        Vector3 velocity = new Vector3 (inputVector.x, rb.velocity.y, inputVector.z);
+        velocity = velocity.normalized * speed;
+       
+        transform.LookAt(transform.position + new Vector3(inputVector.x, 0, inputVector.z));
+
+        rb.velocity = velocity;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    Player.cs
+
+    A class meant to hold the behavioral code for player characters.
+*/
+
+public class Player : Creature
+{
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        // Update the inputVector for movement
+        inputVector.x = Input.GetAxis("Horizontal");
+        inputVector.z = Input.GetAxis("Vertical");
+
+        // Call the update method in the Creature class.
+        // Done after input is retrieved.
+        base.Update();
+    }
+}
