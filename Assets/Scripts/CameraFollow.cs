373377c12689923dd4349d37d08f4d00ef27b6ef
Thomas Pope <45110100+thomasapope@users.noreply.github.com>
2020-02-05 21:54:45 -0600

    Fix camera rotation
    Fixed camera rotation and added some smoothing.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ Script that causes the camera to follow the player.
+*/
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+    private Vector3 velocity = Vector3.zero;
+
+    void LateUpdate() 
+    {
+        Vector3 desiredPosition = target.position + offset;
+        //Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 10f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move() {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        move = move.normalized;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
