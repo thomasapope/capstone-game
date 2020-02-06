239edf5a9ba8ff9b2f69af8dbd75d91e66188b15
Thomas Pope <45110100+thomasapope@users.noreply.github.com>
2020-02-06 17:09:59 -0600

    Added spawning behavior
    Added functioning spawning behavior. There is now a game controller object "_GM" that manages spawning. It has information to track waves and keep the time.

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour 
+{
+    public Transform player;
+    public enum SpawnState { SPAWNING, WAITING, COUNTING }
+
+    [System.Serializable]
+    public class Wave 
+    {
+        public string name;
+        public Enemy enemy;
+        public int count;
+        public float rate;
+    }
+
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public Transform[] spawnPoints;
+
+    public float timeBetweenWaves = 5f;
+    private float waveCountdown;
+
+    private float searchCountdown = 1f;
+
+    private SpawnState state = SpawnState.COUNTING;
+
+
+    void Start() 
+    {
+        // Check if there are spawn points
+        if (spawnPoints.Length == 0)
+        {
+            Debug.Log("Error: No spawn points referenced.");
+        }
+
+        waveCountdown = timeBetweenWaves;
+    }
+
+
+    void Update() 
+    {
+        if (state == SpawnState.WAITING)
+        {
+            // Check if enemies are still alive
+            if (!EnemyIsAlive())
+            {
+                // Begin a new round
+                WaveCompleted();
+            }
+            else 
+            {
+                return;
+            }
+        }
+
+        if (waveCountdown <= 0) 
+        {
+            if (state != SpawnState.SPAWNING) 
+            {
+                // Start spawning wave.
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            }
+        } 
+        else 
+        {
+            waveCountdown -= Time.deltaTime;
+        }
+    }
+
+
+    void WaveCompleted()
+    {
+        Debug.Log("Wave Completed");
+
+        state = SpawnState.COUNTING;
+        waveCountdown = timeBetweenWaves;
+
+        if (nextWave + 1 > waves.Length - 1)
+        {
+            Debug.Log("All Waves Complete! Looping...");
+            nextWave = 0;
+        }
+        else 
+        {
+            nextWave++;
+        }
+    }
+
+    bool EnemyIsAlive()
+    {
+        searchCountdown -= Time.deltaTime;
+        if (searchCountdown <= 0)
+        {
+            searchCountdown = 1f;
+            if (GameObject.FindGameObjectWithTag("Enemy") == null)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    IEnumerator SpawnWave(Wave _wave) 
+    {
+        Debug.Log("Spawning Wave: " + _wave.name);
+        state = SpawnState.SPAWNING;
+
+        // Spawn
+        for(int i = 0; i< _wave.count; i++) 
+        {
+            SpawnEnemy(_wave.enemy);
+            yield return new WaitForSeconds(1f/_wave.rate);
+        }
+
+        state = SpawnState.WAITING;
+
+        yield break;
+    }
+
+
+    void SpawnEnemy(Enemy _enemy)
+    {
+        // Choose a random spawn point to spawn the enemy at.
+        Transform _sp = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+        // Spawn enemy
+        Debug.Log("Spawning Enemy: " + _enemy.name);
+
+        Enemy enemy = Instantiate<Enemy>(_enemy, _sp.position, _sp.rotation);
+        enemy.target = player;
+    }
+
+}
