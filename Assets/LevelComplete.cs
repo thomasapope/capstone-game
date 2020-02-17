e57c980f3b28fcdbb34ca48c34303beb442bf770
Thomas Pope <45110100+thomasapope@users.noreply.github.com>
2020-02-17 11:45:43 -0600

    Add simple win conditions
    Added simple win conditions. Once the player has completed all waves, they are able to step on a platform and win. The game then restarts.

---
+++ Assets/LevelComplete.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelComplete : MonoBehaviour
+{
+   public void LoadNextLevel()
+   {
+       // For now, just restart the level
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+   }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+
+    public float restartDelay = 1f;
+
+    public GameObject completeLevelUI;
+
+
+
+    public void CompleteLevel()
+    {
+        if (WaveSpawner.complete)
+        {
+            Debug.Log("YOU WON!");
+            completeLevelUI.SetActive(true);
+        }
+        else 
+        {
+            Debug.Log("WAVES NOT COMPLETE");
+        }
+    }
+
+    public void EndGame()
+    {
+        if (gameHasEnded == false) 
+        {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER");
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+
+    public static void Restart()
+    {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
