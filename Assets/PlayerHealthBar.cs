6c0c457ef17d54ca7d23a511a223812df975cc69
billyboyks <32182710+billyboyks@users.noreply.github.com>
2020-02-22 18:43:58 -0600

    Added PlayerHealthBar to GUI

---
+++ Assets/PlayerHealthBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthBar : MonoBehaviour
+{
+    public Slider slider;
+    private GameObject player;
+    private Health healthScript;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        healthScript = player.GetComponent<Health>();
+    }
+    
+    void Update()
+    {
+        slider.value = healthScript.hp;
+    }
+}
