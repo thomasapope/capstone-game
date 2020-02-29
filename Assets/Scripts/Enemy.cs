43af1f3380305a686024dc17ae11a788d4cd67f3
Thomas Pope <45110100+thomasapope@users.noreply.github.com>
2020-02-29 15:33:41 -0600

    Enemies now have a small spherical child to steal
    Enemies now steal a spherical "child" and take it back to their spawn point. I need to put a little more work on this to fix some issues I'm pretty sure it has, but otherwise, all is good.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -15,8 +15,12 @@ using UnityEngine.AI;
 // [RequireComponent(typeof(Health))]
 public class Enemy : Creature
 {
+    public Transform startingPoint;
     public Transform target;
     private UnityEngine.AI.NavMeshAgent agent;
+
+    public enum MindState { CHASING, FLEEING }
+    private MindState state = MindState.CHASING;
 
     public float refreshTime = 5f;
     private float timeTilRefresh;
@@ -37,15 +41,20 @@ public class Enemy : Creature
         // Find Target
         if (!target) FindTarget();
 
-        if (timeTilRefresh < 0) // A timer to keep this from getting calculated every frame
-        {
-            FindTarget();
+        if (state == MindState.FLEEING) target = startingPoint;
 
-            timeTilRefresh = refreshTime;
-        }
-        else
+        if (state == MindState.CHASING) 
         {
-            timeTilRefresh -= Time.deltaTime;
+            if (timeTilRefresh < 0) // A timer to keep this from getting calculated every frame
+            {
+                FindTarget();
+
+                timeTilRefresh = refreshTime;
+            }
+            else
+            {
+                timeTilRefresh -= Time.deltaTime;
+            }
         }
 
         float distance = Vector3.Distance(target.position, transform.position);
@@ -54,6 +63,13 @@ public class Enemy : Creature
 
         if (distance <= agent.stoppingDistance)
         {
+            if (target.gameObject.name == "Kid")
+            {
+                Debug.Log("They're taking the children!!!");
+                state = MindState.FLEEING;
+                target.SetParent(transform);
+            }
+
             // Attack
             hitting = true;
             FaceTarget();
@@ -65,28 +81,35 @@ public class Enemy : Creature
 
     private void FindTarget()
     {
-        // Finds the closest target. 
+        // Finds a new target for the enemy. 
         // Should not be executed every frame as it is resource intensive.
-
-        float minDistance = 1000f;
-        Transform closest = transform;
-        foreach (GameObject g in GameManager.targetRefs)
-        {
-            float dist = Vector3.Distance(transform.position, g.transform.position);
-            if (dist < minDistance) 
-            {
-                minDistance = dist;
-                closest = g.transform;
-            }
-        }
-        if (closest)
-        {
-            // Debug.Log(closest.gameObject.name);
-            target = closest;
-        }
-        else 
-        {
+        if (state == MindState.FLEEING) {
+            target = startingPoint;
             return;
+        }
+        
+        if (state == MindState.CHASING)
+        {
+            float minDistance = 1000f;
+            Transform closest = transform;
+            foreach (GameObject g in GameManager.targetRefs)
+            {
+                float dist = Vector3.Distance(transform.position, g.transform.position);
+                if (dist < minDistance) 
+                {
+                    minDistance = dist;
+                    closest = g.transform;
+                }
+            }
+            if (closest)
+            {
+                // Debug.Log(closest.gameObject.name);
+                target = closest;
+            }
+            else 
+            {
+                return;
+            }
         }
     }
 
@@ -98,6 +121,31 @@ public class Enemy : Creature
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * agent.angularSpeed);
     }
 
+
+    protected override void OnDeath()
+    {
+        Debug.Log("Enemy slain!!!");
+
+        // Unparent child if carrying
+        if (state == MindState.FLEEING)
+        {
+            // Debug.Log("Your enemies have dropped a child!!!");
+            // Debug.Log(target.name);
+            Transform[] children = GetComponentsInChildren<Transform>();
+            foreach (Transform child in children)
+            {
+                if (child.CompareTag("Target"))
+                {
+                    child.SetParent(null);
+                }
+            }
+
+            // target.SetParent(null);
+            // target.parent = null;
+        }
+
+        Destroy(gameObject);
+    }
 
     protected override void OnDisable()
     {
