d297576c22a91f4742b819b3f65bc511492b9c57
Thomas Pope <45110100+thomasapope@users.noreply.github.com>
2020-03-23 16:00:12 -0500

    Organize files and dupe my scene
    Organized quite a few files and folders that were getting out of hand and copied the school scene to mine to stay more up to date. Time to get to work.

---
+++ Assets/Scripts/AlertArrow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlertArrow : MonoBehaviour
+{
+    [HideInInspector] public Transform target;
+
+
+    void Update()
+    {
+        // transform.LookAt(new Vector3(target.position.x, transform.position.y, target.position.z));
+        transform.LookAt(target);
+        // Vector3 rotation = bullet.transform.rotation.eulerAngles;
+        // bullet.transform.rotation = Quaternion.Euler(rotation.x, transform.eulerAngles.y, rotation.z);
+    }
+}
---
+++ Assets/Scripts/AlertArrowController.cs
@@ -0,0 +1,41 @@
+﻿/*
+    AlertArrow.cs
+
+    Used to point the direction of important events, such
+    as an enemy picking up and carrying away a child.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlertArrowController : MonoBehaviour
+{
+    public GameObject arrowPrefab;
+
+
+    void Start()
+    {
+        // Create new alert arrow when the delegate gets called
+        Child.ChildPickedUp += CreateAlertArrow;
+
+
+        // Creature.OnHealthAdded += AddHealthBar;
+        // Creature.OnHealthRemoved += RemoveHealthBar;
+        // arrow = 
+
+        // bullet.transform.position = firePoint.position;
+        // Vector3 rotation = bullet.transform.rotation.eulerAngles;
+        // bullet.transform.rotation = Quaternion.Euler(rotation.x, transform.eulerAngles.y, rotation.z);
+    }
+
+
+    public void CreateAlertArrow(Transform location)
+    {
+        GameObject arrow = Instantiate(arrowPrefab); // create the arrow
+        arrow.transform.SetParent(transform); // reparent it so it moves along with the player
+        arrow.transform.position = transform.position; // position it properly
+        arrow.GetComponent<AlertArrow>().target = location;
+
+    }
+}
