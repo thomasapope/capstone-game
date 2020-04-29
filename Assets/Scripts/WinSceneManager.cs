c812bd8e066448448dcb2fdf676ac93fcc854c61
billyboyks <32182710+billyboyks@users.noreply.github.com>
2020-04-28 21:58:36 -0500

    Added Music and Sounds for EndScene

---
+++ Assets/Scripts/WinSceneManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinSceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Play Music
+        FindObjectOfType<AudioManager>().Play("Win");
+    }
+}
